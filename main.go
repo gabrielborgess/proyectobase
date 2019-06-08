bdacf0209a3b9120977c5c7f9a0ce45c4b24aa0b
Gabriel Borges <gbordesc@uft.edu>
2019-06-08 13:12:00 -0400

    Ultimos cambios

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -10,8 +11,27 @@ func main() {
 	db, err := sql.Open("mysql", "admin_admin:ganzo10.@tcp(158.69.60.190:22)/admin_proyecto")
 
 	if err != nil {
-		panic(err.Error())
+		fmt.Printf("error al conectar")
 	}
 
+	fmt.Println("Base de datos conectada exitosamente")
+
 	defer db.Close()
+
+	dato, err2 := db.Query("select * from tablaprueba")
+	if err2 != nil {
+		fmt.Println("error prro")
+	}
+
+	for dato.Next() {
+		var rut string
+		err3 := dato.Scan(&rut)
+
+		if err3 != nil {
+			fmt.Println("error prro2")
+		}
+		fmt.Println(rut)
+
+	}
+
 }
