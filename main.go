f9c95de7ea0b78230757e1eec734ee6c6a59b7d3
Nicolás Hidalgo <oxakromax@hotmail.com>
2019-06-23 22:22:48 -0500

    gg

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() { // Esta muy feo, apenas aprenda a iterar listas aqui lo arreglo
 
 	fmt.Println("Conexion exitosa")
 	execdb("DROP TABLE IF EXISTS `clientes`")
-	execdb("CREATE TABLE clientes ( id integer, data varchar(255) )")
+	execdb("CREATE TABLE clientes ( id integer, data varchar(250) )")
 	execdb("DROP TABLE IF EXISTS `vendedores`")
 	execdb("CREATE TABLE vendedores ( id integer, data varchar(255) )")
 	execdb("DROP TABLE IF EXISTS `pedidos`")
