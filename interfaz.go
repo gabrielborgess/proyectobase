d5976bab133ac5b13b062e4d3b94a07936c48fda
Gabriel Borges <gbordesc@uft.edu>
2019-06-28 21:41:46 -0400

    Merge branch 'master' of C:\Users\gabri\Desktop\ProyectoBasedeDatos with conflicts.

--- interfaz.go
+++ interfaz.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
-func Interfaz() {
-	fmt.Printf("hola")
+func main() {
+	xd := "<joli>"
+	opcion := "<Selecciona una opcion:>"
+	fmt.Println("Bienvenido al administrador de videojuegos")
+	fmt.Println("Que deseas hacer?.\n")
+	fmt.Println("1.Añadir un videojuego")
+	fmt.Println("2.Editar videojuego")
+	fmt.Println("3.Buscar videojuego")
+	fmt.Println("4.Salir\n")
+	fmt.Print("Selecciona una opcion:")
+	fmt.Scanf("%s", &opcion)
+	fmt.Print("Selecciona una opcion:")
+	fmt.Scanf("%s", &xd)
 }
--- main.go
+++ main.go
@@ -21,27 +21,63 @@ func execdb(a string) {
 	fmt.Println("Query ejecutada correctamente")
 }
 
-func main() { // Esta muy feo, apenas aprenda a iterar listas aqui lo arreglo
+func main() {
 
-	fmt.Println("Conexion exitosa")
+	//Interfaz()
+	db, err := sql.Open("mysql", "admin_admin:ganzo10.@tcp(158.69.60.190:3306)/admin_proyecto")
+	if err != nil {
+		fmt.Printf("error al conectar")
+		return
+	}
+
+	fmt.Println("Base de datos conectada exitosamente")
+
+	defer db.Close()
 	execdb("DROP TABLE IF EXISTS `clientes`")
-	execdb("CREATE TABLE clientes ( id integer, data varchar(255) )")
-	execdb("DROP TABLE IF EXISTS `vendedores`")
-	execdb("CREATE TABLE vendedores ( id integer, data varchar(255) )")
-	execdb("DROP TABLE IF EXISTS `pedidos`")
-	execdb("CREATE TABLE pedidos( id integer, data varchar(255) )")
-	execdb("DROP TABLE IF EXISTS `detalle_pedidos`")
-	execdb("CREATE TABLE detalle_pedidos ( id integer, data varchar(255) )")
-	execdb("DROP TABLE IF EXISTS `productos`")
-	execdb("CREATE TABLE productos ( id integer, data varchar(255) )")
-	execdb("DROP TABLE IF EXISTS `proveedores`")
-	execdb("CREATE TABLE proveedores( id integer, data varchar(255) )")
-	execdb("DROP TABLE IF EXISTS `empleados`")
-	execdb("CREATE TABLE empleados ( id integer, data varchar(255) )")
-	execdb("DROP TABLE IF EXISTS `genero`")
-	execdb("CREATE TABLE genero ( id integer, data varchar(255) )")
-	execdb("DROP TABLE IF EXISTS `metodo_pago`")
-	execdb("CREATE TABLE metodo_pago ( id integer, data varchar(255) )")
+	_, err = db.Exec("CREATE TABLE clientes ( id integer, data varchar(32) )")
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.Exec("DROP TABLE IF EXISTS `vendedores`")
+	_, err = db.Exec("CREATE TABLE vendedores ( id integer, data varchar(32) )")
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.Exec("DROP TABLE IF EXISTS `pedidos`")
+	_, err = db.Exec("CREATE TABLE pedidos( id integer, data varchar(32) )")
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.Exec("DROP TABLE IF EXISTS `detalle_pedidos`")
+	_, err = db.Exec("CREATE TABLE detalle_pedidos ( id integer, data varchar(32) )")
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.Exec("DROP TABLE IF EXISTS `productos`")
+	_, err = db.Exec("CREATE TABLE productos ( id integer, data varchar(32) )")
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.Exec("DROP TABLE IF EXISTS `proveedores`")
+	_, err = db.Exec("CREATE TABLE proveedores( id integer, data varchar(32) )")
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.Exec("DROP TABLE IF EXISTS `empleados`")
+	_, err = db.Exec("CREATE TABLE empleados ( id integer, data varchar(32) )")
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.Exec("DROP TABLE IF EXISTS `genero`")
+	_, err = db.Exec("CREATE TABLE genero ( id integer, data varchar(32) )")
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.Exec("DROP TABLE IF EXISTS `metodo_pago`")
+	_, err = db.Exec("CREATE TABLE metodo_pago ( id integer, data varchar(32) )")
+	if err != nil {
+		panic(err)
+	}
 
 	fmt.Println("Tablas Creadas correctamente")
 
